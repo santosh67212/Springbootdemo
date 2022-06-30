9c4348fc725644bc98a55f3c9a76e5b923507c37
santosh67212 <105358745+santosh67212@users.noreply.github.com>
2022-06-30 09:27:54 +0530

    Add files via upload

---
+++ department-crud/src/main/java/com/zensar/service/DepartmentServiceImpl.java
@@ -0,0 +1,36 @@
+package com.zensar.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.zensar.entity.Department;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService {
+
+	@Override
+	public Department saveDepartment(Department department) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> fetchDepartmentList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Department updateDepartment(Department department, Long departmentId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteDepartmentById(Long departmentId) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
