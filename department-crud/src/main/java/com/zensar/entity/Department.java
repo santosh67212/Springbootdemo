11e919f3a53f4658530f7314a6bc7994d8f489aa
santosh67212 <105358745+santosh67212@users.noreply.github.com>
2022-07-01 09:07:33 +0530

    Add files via upload

---
+++ department-crud/src/main/java/com/zensar/entity/Department.java
@@ -0,0 +1,59 @@
+package com.zensar.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Department {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+
+	private long departmentId;
+	private String departmentName;
+	private String departmentAddress;
+
+	public Department() {
+		super();
+	}
+
+	public Department(long departmentId, String departmentName, String departmentAddress) {
+		super();
+		this.departmentId = departmentId;
+		this.departmentName = departmentName;
+		this.departmentAddress = departmentAddress;
+	}
+
+	public long getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(long departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+
+	public String getDepartmentAddress() {
+		return departmentAddress;
+	}
+
+	public void setDepartmentAddress(String departmentAddress) {
+		this.departmentAddress = departmentAddress;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [departmentId=" + departmentId + ", departmentName=" + departmentName
+				+ ", departmentAddress=" + departmentAddress + "]";
+	}
+
+}
